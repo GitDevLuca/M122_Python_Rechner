5c1b308720f85e9a4d93c0829379b6b7d7915e40
Tree™ <133748737+GitDevLuca@users.noreply.github.com>
2023-06-06 14:48:56 +0200

    Update Rechner.py

--- Rechner.py
+++ Rechner.py
@@ -1,63 +1,63 @@
-#Dies is ein kleiner Taschenrechner 🌐
+import sys
 
-# Funktionen Definieren, Plus, Minus, Mal, Durch
-def Addition(x, y):
-    return x + y
-def Subtraktion(x, y):
-    return x - y
-def Multiplikation(x, y):
-    return x * y
-def Division(x, y):
-    return x / y
+def start_from_beginning():
+    # Dies ist ein kleiner Taschenrechner 🌐
+
+    # Funktionen definieren: Plus, Minus, Mal, Durch
+    def Addition(x, y):
+        return x + y
+
+    def Subtraktion(x, y):
+        return x - y
+
+    def Multiplikation(x, y):
+        return x * y
+
+    def Division(x, y):
+        return x / y
+
+    print("Taschenrechner")
+
+    while True:
+        print("Bitte wählen Sie eine Operation aus:")
+        print("1. Addieren")
+        print("2. Subtrahieren")
+        print("3. Multiplizieren")
+        print("4. Dividieren")
+        print("5. Beenden")
+
+        Operation = input("Auswahl: ")
+
+        if Operation not in ["1", "2", "3", "4", "5"]:
+            print("Ungültige Eingabe. Bitte wählen Sie eine Zahl von 1-5.")
+            continue
+
+        if Operation == '5':
+            print("Programm beendet.")
+            break
+
+        try:
+            Eingabe1 = float(input("Geben Sie die erste Zahl ein: "))
+            Eingabe2 = float(input("Geben Sie die zweite Zahl ein: "))
+        except ValueError:
+            print("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.")
+            continue
+
+        if Operation == "1":
+            Resultat = Addition(Eingabe1, Eingabe2)
+        elif Operation == "2":
+            Resultat = Subtraktion(Eingabe1, Eingabe2)
+        elif Operation == "3":
+            Resultat = Multiplikation(Eingabe1, Eingabe2)
+        elif Operation == "4":
+            if Eingabe2 == 0:
+                print("Fehler: Division durch Null ist nicht erlaubt.")
+                continue
+            Resultat = Division(Eingabe1, Eingabe2)
+
+        print("Das Ergebnis ist:", Resultat)
+        print("")
 
 
-# Inputs vom User für die Berechnung fordern
 
-print("Taschenrechner")
-
-
-
-#Hier kann man auswählen welche Operation benutzt werden soll
-while True:
-    print("Bitte wählen Sie eine Operation aus:")
-    print("1. Addieren")
-    print("2. Subtraahieren")
-    print("3. Multiplizieren")
-    print("4. Dividieren")
-    print("5. Beenden")
-
-#In dieser Variable wird gespeichert welche eingabe getätigt wurde
-    Operation = input("Auswahl: ")
-    
-    
-
-#Wenn 5 gewählt wird, wird der Taschenrechner gestoppt
-    if Operation == '5':
-        print("Programm beendet.")
-        break
-
-# Hier werden 2 Eingaben vom Benutzer gefordert um jeweils damit zu rechnen
-    Eingabe1 = float(input("Gib die erste Zahl ein: "))
-    Eingabe2 = float(input("Gib die zweite Zahl ein: "))
-
-
-#Hier werden die einzelnen berechnungen durchgeführt
-    if Operation == "1":
-        Resultat = Addition(Eingabe1, Eingabe2)
-
-
-    elif Operation == "2":
-        Resultat = Subtraktion(Eingabe1, Eingabe2)
-
-
-    elif Operation == "3":
-        Resultat = Multiplikation(Eingabe1, Eingabe2)
-
-
-    elif Operation == "4":
-        Resultat = Division(Eingabe1, Eingabe2)
-
-
-#Anschliessend werden diese in der Variable "Resultat" gespeichert & Hier ausgegeben
-    print("Das Ergebnis Ist:", Resultat)
-    print("")
+start_from_beginning()
