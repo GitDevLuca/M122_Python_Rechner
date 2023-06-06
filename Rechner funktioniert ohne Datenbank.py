1a7997299d95e06ad3628524a7520a504aa1df7a
Tree™ <133748737+GitDevLuca@users.noreply.github.com>
2023-06-06 14:55:57 +0200

    Add files via upload

---
+++ Rechner funktioniert ohne Datenbank.py
@@ -0,0 +1,67 @@
+import sys
+
+def start_from_beginning():
+    # Dies ist ein kleiner Taschenrechner 🌐
+
+    # Funktionen definieren: Plus, Minus, Mal, Durch
+    def Addition(x, y):
+        return x + y
+
+    def Subtraktion(x, y):
+        return x - y
+
+    def Multiplikation(x, y):
+        return x * y
+
+    def Division(x, y):
+        return x / y
+
+    print("Taschenrechner")
+
+    while True:
+        print("Bitte wählen Sie eine Operation aus:")
+        print("1. Addieren")
+        print("2. Subtrahieren")
+        print("3. Multiplizieren")
+        print("4. Dividieren")
+        print("5. Beenden")
+
+        Operation = input("Auswahl: ")
+
+        if Operation not in ["1", "2", "3", "4", "5"]:
+            print("Ungültige Eingabe. Bitte wählen Sie eine Zahl von 1-5.")
+            continue
+
+        if Operation == '5':
+            print("Programm beendet.")
+            break
+
+        try:
+            Eingabe1 = float(input("Geben Sie die erste Zahl ein: "))
+            Eingabe2 = float(input("Geben Sie die zweite Zahl ein: "))
+        except ValueError:
+            print("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.")
+            continue
+
+        if Operation == "1":
+            Resultat = Addition(Eingabe1, Eingabe2)
+        elif Operation == "2":
+            Resultat = Subtraktion(Eingabe1, Eingabe2)
+        elif Operation == "3":
+            Resultat = Multiplikation(Eingabe1, Eingabe2)
+        elif Operation == "4":
+            if Eingabe2 == 0:
+                print("Fehler: Division durch Null ist nicht erlaubt.")
+                continue
+            Resultat = Division(Eingabe1, Eingabe2)
+
+        print("Das Ergebnis ist:", Resultat)
+        print("")
+
+        # Nach dem Berechnen fragen, ob eine weitere Berechnung durchgeführt werden soll
+        choice = input("Möchten Sie eine weitere Berechnung durchführen? (ja/nein): ")
+        if choice.lower() != 'ja':
+            print("Programm beendet.")
+            break
+
+start_from_beginning()
